7a9b71bc44101cd481c745175d33842eaf0c7533
Jost Tobias Springenberg <springj@informatik.uni-freiburg.de>
2015-12-17 01:22:16 +0100

    cherry pick dataset from previous commit and add exercise3

--- nn/data.py
+++ nn/data.py
@@ -41,3 +41,40 @@ def mnist(datasets_dir='./data'):
     print('... done loading data')
     return rval
 
+def minicifar10(datasets_dir='./data'):
+    if not os.path.exists(datasets_dir):
+        os.mkdir(datasets_dir)
+    data_file = os.path.join(datasets_dir, 'mini_cifar10.pkl.gz')
+    if not os.path.exists(data_file):
+        print('... downloading mini CIFAR-10 from the web')
+        try:
+            import urllib
+            urllib.urlretrieve('http://google.com')
+        except AttributeError:
+            import urllib.request as urllib
+        url = 'http://www.wrfl.de/serve/mini_cifar10.pkl.gz'
+        urllib.urlretrieve(url, data_file)
+
+    print('... loading data')
+    # Load the dataset
+    f = gzip.open(data_file, 'rb')
+    try:
+        train_set, valid_set, test_set = cPickle.load(f, encoding="latin1")
+    except TypeError:
+        train_set, valid_set, test_set = cPickle.load(f)
+    f.close()
+
+    test_x, test_y = test_set
+    test_x = test_x.astype('float32')
+    test_x = test_x.astype('float32').reshape(test_x.shape[0], 3, 32, 32)
+    test_y = np.asarray(test_y, dtype=np.int32)
+    valid_x, valid_y = valid_set
+    valid_x = valid_x.astype('float32')
+    valid_x = valid_x.astype('float32').reshape(valid_x.shape[0], 3, 32, 32)
+    valid_y = np.asarray(valid_y, dtype=np.int32)
+    train_x, train_y = train_set
+    train_x = train_x.astype('float32').reshape(train_x.shape[0], 3, 32, 32)
+    train_y = np.asarray(train_y, dtype=np.int32)
+    rval = [(train_x, train_y), (valid_x, valid_y), (test_x, test_y)]
+    print('... done loading data')
+    return rval
