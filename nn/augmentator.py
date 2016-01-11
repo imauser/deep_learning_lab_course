d96cfa2446a5d89dceaaa438b75fcc6949062780
ihopeidontforgetthisaccname <johnvergeben@gmail.com>
2016-01-11 12:28:41 +0100

    Added augmentations to the cifar set.

--- nn/augmentator.py
+++ nn/augmentator.py
@@ -14,7 +14,7 @@ class Augmentator():
         for i in images:
             for j in range(zooms):
                 out.append(Augmentator.augment_keeping_image_size(Augmentator.zoom, i,\
-                        zoom=[1+random.random(), 1+random.random(), 1]))
+                        zoom=[1, 1+random.random(), 1+random.random()]))
             for j in range(eds):
                 out.append(Augmentator.augment_keeping_image_size(\
                         Augmentator.elastic_deformation, i, sigma=2*random.random(),\
@@ -22,15 +22,20 @@ class Augmentator():
         return out
 
     @staticmethod
-    def zoom(image, zoom=[1.5,1.5,1], *args, **kwargs):
+    def rotate(image, angle, *args, **kwargs):
+        return nd.interpolation.rotate(image, angle, *args, **kwargs)
+
+
+    @staticmethod
+    def zoom(image, zoom=[1,1.5,1.5], *args, **kwargs):
         return nd.interpolation.zoom(image, zoom, *args, **kwargs)
 
     @staticmethod
     def augment_keeping_image_size(func, image, *args, **kwargs):
-        shape = (image.shape[0], image.shape[1])
+        shape = (image.shape[1], image.shape[2])
         new_image = func(image, *args, **kwargs)
-        offset = (new_image.shape[0] - shape[0])/2, (new_image.shape[0] - shape[0])/2
-        p = new_image[offset[0] : offset[0] + shape[0], offset[1] : offset[1] + shape[1]]
+        offset = (new_image.shape[1] - shape[0])/2, (new_image.shape[2] - shape[1])/2
+        p = new_image[:, offset[0] : offset[0] + shape[0], offset[1] : offset[1] + shape[1]]
         return p
 
     @staticmethod
@@ -39,24 +44,21 @@ class Augmentator():
                 *args, **kwargs)
 
     @staticmethod
-    def elastic_deformation(image, sigma=1.,alpha=100., random_state=None):
+    def elastic_deformation(image, sigma=1.,alpha=100.):
         """
         Caution only works on 3 layer images
         """
 
-        if random_state is None:
-            random_state = np.random.RandomState(None)
+        random_state = np.random.RandomState(None)
 
         # create a field with new coordinate positions
-        shape = (image.shape[0], image.shape[1])
-        print (image.shape[0])
+        shape = (image.shape[1], image.shape[2])
         dx = alpha * nd.filters.gaussian_filter((random_state.rand(*shape)\
                 * 2 - 1), sigma, mode="constant")
         dy = alpha * nd.filters.gaussian_filter((random_state.rand(*shape)\
                 * 2 - 1), sigma, mode="constant")
 
         x, y = np.meshgrid(np.arange(shape[1]), np.arange(shape[0]))
-        print(x)
         indices =  np.reshape(y+dy, (-1, 1)), np.reshape(x+dx, (-1, 1))
 
         # map coordinates does not want multi channel images
@@ -64,9 +66,9 @@ class Augmentator():
             return nd.interpolation.map_coordinates(image, indices, order=1).reshape(shape)
 
         im = []
-        for c in range(len(image.shape)):
-            im.append(nd.interpolation.map_coordinates(image[:,:,c], indices,\
+        for c in range(image.shape[0]):
+            im.append(nd.interpolation.map_coordinates(image[c,:,:], indices,\
                     order=1).reshape(shape))
 
         im = np.array(im)
-        return im.transpose(1,2,0)
+        return im
---
+++ run.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python2.7
+"""
+Run Network
+"""
+import numpy as np
+import nn
+import time
+# import matplotlib.pyplot as plt
+
+# you can load the cifar10 data like this
+data = nn.data.minicifar10()
+train_data, valid_data, test_data = data
+print("data size: " + str(len(data[0][0])))
+train_x, train_y = train_data
+valid_x, valid_y = valid_data
+test_x, test_y = test_data
+
+train_x = train_x.astype(np.float_) / 255.
+train_y = train_y.astype(np.float_)
+valid_x = valid_x.astype(np.float_) / 255.
+valid_y = valid_y.astype(np.float_)
+test_x = test_x.astype(np.float_) / 255.
+test_y = test_y.astype(np.float_)
+
+print(train_x.shape)
+print(train_y.shape)
+train_x = np.append(train_x, nn.Augmentator.augment_images(train_x, 2), axis=0)
+
+train_y = np.append(train_y, np.append(train_y,train_y, axis=0), axis=0)
+print(train_x.shape)
+print(train_y.shape)
+
+input_shape = (None, 3, 32, 32)
+layers = [nn.InputLayer(input_shape)]
+layers.append(nn.Conv(
+              layers[-1],
+              n_feats=32,
+              filter_shape=(5, 5),
+              init_stddev=0.1,
+              activation_fun=nn.layers.Activation('relu')))
+
+layers.append(nn.Pool(layers[-1]))
+
+layers.append(nn.Conv(
+              layers[-1],
+              n_feats=32,
+              filter_shape=(5, 5),
+              init_stddev=0.1,
+              activation_fun=nn.layers.Activation('relu')))
+
+layers.append(nn.Pool(layers[-1]))
+
+layers.append(nn.Conv(
+              layers[-1],
+              n_feats=64,
+              filter_shape=(5, 5),
+              init_stddev=0.1,
+              activation_fun=nn.layers.Activation('relu')))
+
+layers.append(nn.Flatten(layers[-1]))
+
+layers.append(nn.FullyConnectedLayer(
+              layers[-1],
+              num_units=64,
+              init_stddev=0.1,
+              activation_fun=nn.layers.Activation('relu')))
+
+layers.append(nn.FullyConnectedLayer(
+              layers[-1],
+              num_units=10,
+              init_stddev=0.1,
+              activation_fun=nn.layers.Activation('linear')))
+
+layers.append(nn.FullyConnectedLayer(
+              layers[-1],
+              num_units=10,
+              init_stddev=0.1,
+              activation_fun=None))
+layers.append(nn.SoftmaxOutput(layers[-1]))
+
+net = nn.NeuralNetwork(layers)
+
+start_train_time = time.time()
+
+net.train(train_x, train_y, Xvalid=valid_x,
+          Yvalid=valid_y, learning_rate=0.01,
+          max_epochs=100, batch_size=128,
+          y_one_hot=True)
+end_train_time = time.time()
+
+print("Time: {:.2f}".format(end_train_time - start_train_time))
